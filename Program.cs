6052b23dbf36ceb85c1b04199d2617511a221397
Candace <goodsoncj@gmail.com>
2020-09-06 15:09:45 -0400

    Start difficulty functionality

--- Program.cs
+++ Program.cs
@@ -14,7 +14,8 @@ namespace guessingGame
 
              3. Create a loop to always start the game over once the right number is guessed. - COMPLETE
 
-             4. Ask the user what difficult level they would like and give the user a message that they used too many guesses.
+             4. Ask the user what difficult level they would like and give the user a message that they used too many guesses. --- FIX OPTION for UNLIMITED GUESSES
+                    Other options (2 guesses, 4 guesses) are COMPLETE
 
              5. Add an "easter egg" that if the user types in a certain phrase, the user gets a message back. 
 
@@ -22,7 +23,7 @@ namespace guessingGame
 
             bool realInteger = false;
             bool realIntMin = false;
-            bool realIntMax = false; 
+            bool realIntMax = false;
             int userNumberGuess = 0;
             bool correctGuess = false; //hold value if user guessed correctly
             //default is false otherwise the while loop will assume true and quit the game
@@ -34,16 +35,25 @@ namespace guessingGame
             int userRangeMax = 0;
             int numberGuesses = 0;
             bool validInput = false; //validate user inputs
+            int gameCount = 1;
 
             //best practice to welcome user before starting game
             Console.WriteLine("Welcome to the Number Guessing Game!");
 
             while (true)
             {
+                if (gameCount > 1)
+                {
+                    Console.ForegroundColor = ConsoleColor.Green;
+                    Console.WriteLine("\nLET'S PLAY AGAIN!");
+                    Console.ForegroundColor = ConsoleColor.White;
+                }
+
                 while (validInput == false)
                 {
                     Console.ForegroundColor = ConsoleColor.White;
 
+                    Console.WriteLine("\nROUND " + (gameCount) + "!");
                     Console.Write("\nPlease choose your number range to guess in by first choosing the smallest number in the range: ");
 
                     string userInputMin = Console.ReadLine();
@@ -53,7 +63,7 @@ namespace guessingGame
                     if (realIntMin == false)
                     {
                         Console.ForegroundColor = ConsoleColor.Red; //changes font color...will have to change back after.
-                        Console.WriteLine("\nSorry, the number needs to be a whole number. \n");
+                        Console.WriteLine("\nSorry, the input needs to be a whole number. \n");
                         continue; //this will stop the program from going to the next if statements since data not valid
                         Console.ForegroundColor = ConsoleColor.White;
                     }
@@ -80,9 +90,9 @@ namespace guessingGame
 
                     if (realIntMax == false)
                     {
-                        Console.ForegroundColor = ConsoleColor.Red; //changes font color...will have to change back after.
-                        Console.WriteLine("\nSorry, the number needs to be a whole number. \n");
-                        continue; //this will stop the program from going to the next if statements since data not valid
+                        Console.ForegroundColor = ConsoleColor.Red; 
+                        Console.WriteLine("\nSorry, the input needs to be a whole number. \n");
+                        continue; 
                         Console.ForegroundColor = ConsoleColor.White;
                     }
                     else
@@ -95,6 +105,57 @@ namespace guessingGame
                 }
 
                 int randomNumber = rand.Next(userRangeMin, userRangeMax + 1);
+
+                validInput = false;
+                int gameLevel = 0;
+                bool realIntLvl = false;
+                bool noLimit = false;
+
+                //add game difficulty here
+                while (validInput == false)
+                {
+                    Console.ForegroundColor = ConsoleColor.White;
+
+                    Console.Write("\nPlease choose a game difficulty level from the list below: \n\n" +
+                        "1. Unlimited attempts\n" +
+                        "2. 4 attempts\n" +
+                        "3. 2 attempts\n\n");
+
+                    string userLevel = Console.ReadLine();
+
+                    realIntLvl = int.TryParse(userLevel, out gameLevel);
+
+                    if (realIntLvl == false)
+                    {
+                        Console.ForegroundColor = ConsoleColor.Red; 
+                        Console.WriteLine("\nSorry, the input needs to be a whole number. \n");
+                        continue; //this will stop the program from going to the next if statements since data not valid
+                        Console.ForegroundColor = ConsoleColor.White;
+                    }
+                    else
+                    {
+                        validInput = true;
+                        //testing that we have an correct input for user game level
+                        //Console.WriteLine(gameLevel);
+
+                    }
+
+                }
+
+                int limitOfGuesses = 0;
+
+                if (gameLevel == 3)
+                {
+                    limitOfGuesses = 2;
+
+                } else if (gameLevel == 2) {
+
+                    limitOfGuesses = 4;
+
+                } else if (gameLevel == 1)
+                {
+                    noLimit = true;
+                }
 
                 //TESTING:
                 //Console.WriteLine("MIN = " + userRangeMin);
@@ -115,9 +176,9 @@ namespace guessingGame
 
                     if (userNumberGuess > userRangeMax || userNumberGuess < userRangeMin || realInteger == false)
                     {
-                        Console.ForegroundColor = ConsoleColor.Red; //changes font color...will have to change back after.
+                        Console.ForegroundColor = ConsoleColor.Red; 
                         Console.WriteLine("\nSorry, the number needs to be a whole number between " + userRangeMin + " and " + userRangeMax + ".\n");
-                        continue; //this will stop the program from going to the next if statements since data not valid
+                        continue; 
                         Console.ForegroundColor = ConsoleColor.White;
                     }
 
@@ -126,25 +187,44 @@ namespace guessingGame
                     {
                         Console.WriteLine("\nThe Number is Greater than " + userNumberGuess);
                         numberGuesses = numberGuesses + 1;
+
+                        if (numberGuesses >= limitOfGuesses)
+                        {
+                            Console.ForegroundColor = ConsoleColor.Red;
+                            Console.WriteLine("\nYou've run out of chances. GAME OVER!");
+                            correctGuess = true;
+                            validInput = false;
+                        } 
+
                     }
                     else if (randomNumber < userNumberGuess)
                     {
                         Console.WriteLine("\nThe Number is less than " + userNumberGuess);
                         numberGuesses = numberGuesses + 1;
+
+                        if (numberGuesses >= limitOfGuesses)
+                        {
+                            Console.ForegroundColor = ConsoleColor.Red;
+                            Console.WriteLine("\nYou've run out of chances. GAME OVER!");
+                            correctGuess = true;
+                            validInput = false;
+                        }
                     }
                     else
                     {
                         numberGuesses = numberGuesses + 1;
                         Console.ForegroundColor = ConsoleColor.Green;
                         Console.WriteLine("\nGood Job! The Correct Number is " + randomNumber);
-                        Console.WriteLine("\n It took " + numberGuesses + " guesses to answer correctly!");
+                        Console.WriteLine("\nIt took " + numberGuesses + " attempt(s) to answer correctly!");
                         correctGuess = true;
                         validInput = false; //for the game restart
 
                     }
+                    
 
                 }
 
+                gameCount += 1;
             }
            
             
